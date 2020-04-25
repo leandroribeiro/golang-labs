a245c13dab01caa9fa13ab7f649ca8788b952fa7
Leandro Ribeiro <gitkraken@leandroribeiro.com>
2020-04-24 22:19:53 -0300

    POC API 3, implementando testes do login controller

---
+++ api-lab3/tests/controllertests/login_controller_test.go
@@ -0,0 +1,56 @@
+package controllertests
+
+import (
+	"errors"
+	"fmt"
+	"github.com/prometheus/common/log"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestSignIn(t *testing.T) {
+
+	err := refreshUserTable()
+	if err != nil {
+		log.Fatal(err)
+	}
+	user, err := seedOneUser()
+	if err != nil {
+		fmt.Printf("This is the err %v\n", err)
+	}
+
+	samples := []struct {
+		email        string
+		password     string
+		errorMessage string
+	}{
+		{
+			email:        user.Email,
+			password:     "password",
+			errorMessage: "",
+		},
+		{
+			email:        user.Email,
+			password:     "Wrong password",
+			errorMessage: "crypto/bcrypt: hashedPassword is not the hash of the given password",
+		},
+		{
+			email:        "Wrong email",
+			password:     "password",
+			errorMessage: "record not found",
+		},
+	}
+
+	for _, v := range samples {
+		token, err := server.SignIn(v.email, v.password)
+		if err != nil {
+			assert.Equal(t, err, errors.New(v.errorMessage))
+		} else {
+			assert.NotEqual(t, token, "")
+		}
+	}
+}
+
+func TestLogin(t *testing.T) {
+	//TODO
+}
