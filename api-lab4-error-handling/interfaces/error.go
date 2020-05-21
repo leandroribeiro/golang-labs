96ce26b600aa28ce3426accee4f4bbcd0a2d1f07
Leandro Ribeiro <>
2020-05-21 14:17:17 -0300

    POC API 4, refactor simplificando tratamento de erros

---
+++ api-lab4-error-handling/interfaces/error.go
@@ -0,0 +1,13 @@
+package interfaces
+
+// ClientError is an error whose details to be shared with client.
+type ClientError interface {
+	Error() string
+	// ResponseBody returns response body.
+	ResponseBody() ([]byte, error)
+	// ResponseHeaders returns http status code and headers.
+	ResponseHeaders() (int, map[string]string)
+}
+
+
+
--- api-lab4-error-handling/main.go
+++ api-lab4-error-handling/main.go
@@ -3,6 +3,9 @@ package main
 import (
 	"encoding/json"
 	"errors"
+	"fmt"
+	"github.com/leandroribeiro/golang-labs/api-lab4-error-handling/infrastructure"
+	"github.com/leandroribeiro/golang-labs/api-lab4-error-handling/interfaces"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -27,43 +30,77 @@ func loginUser(username string, password string) (bool, error) {
 	return false, errors.New("Jedis are trying invade!")
 }
 
-func loginHandler(w http.ResponseWriter, r *http.Request) {
+type rootHandler func(http.ResponseWriter, *http.Request) error
+
+func loginHandler(w http.ResponseWriter, r *http.Request) error {
+
 	if r.Method != http.MethodPost {
-		w.WriteHeader(405) // Return 405 Method Not Allowed.
-		return
+		return infrastructure.NewHTTPError(nil, 405, "Method not allowed.")
 	}
+
 	// Read request body.
 	body, err := ioutil.ReadAll(r.Body)
 	if err != nil {
-		log.Printf("Body read error, %v", err)
-		w.WriteHeader(500) // Return 500 Internal Server Error.
-		return
+		return fmt.Errorf("Request body read error: %v", err)
 	}
 
 	// Parse body as json.
 	var schema loginSchema
 	if err = json.Unmarshal(body, &schema); err != nil {
-		log.Printf("Body parse error, %v", err)
-		w.WriteHeader(400) // Return 400 Bad Request.
-		return
+		return infrastructure.NewHTTPError(err, 400, "Bad request: invalid JSON")
 	}
 
 	ok, err := loginUser(schema.Username, schema.Password)
 	if err != nil {
-		log.Printf("Login user DB error, %v", err)
-		w.WriteHeader(500) // Return 500 Internal Server Error.
+		return fmt.Errorf("loginUser DB error: %v", err)
+	}
+
+	if !ok { // Authentication failed.
+		return infrastructure.NewHTTPError(nil, 401, "Wrong password or username")
+	}
+
+	w.WriteHeader(200) // Successfully authenticated. Return access token?
+	return nil
+}
+
+func (fn rootHandler) ServeHTTP(w http.ResponseWriter, r *http.Request){
+	err := fn(w, r) // Call handler function
+	if err == nil {
 		return
 	}
 
-	if !ok {
-		log.Printf("Unauthorized access for user: %v", schema.Username)
-		w.WriteHeader(401) // Wrong password or username, Return 401.
+	// This is where our error handling logic starts
+	log.Printf("An error accured: %v", err)// Log the error
+
+	clientError, ok := err.(interfaces.ClientError) // Check if it is a ClientError.
+	if !ok{
+		// If the error is not ClientError, assume that it is ServerError.
+		w.WriteHeader(500)// return 500 Internal Server Error
 		return
 	}
-	w.WriteHeader(200) // Successfully logged in.
+
+	body, err := clientError.ResponseBody()
+	if err != nil {
+		log.Printf("An error accured: %v", err)
+		w.WriteHeader(500)
+		return
+	}
+
+	status, headers := clientError.ResponseHeaders()
+	for k, v := range headers {
+		w.Header().Set(k, v)
+	}
+
+	w.WriteHeader(status)
+	w.Write(body)
+
 }
 
 func main() {
-	http.HandleFunc("/login/", loginHandler)
+	// Notice rootHandler
+	http.Handle("/login/", rootHandler(loginHandler))
+
+	fmt.Println("Server is ready and listening :)")
+
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
